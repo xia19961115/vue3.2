a72d037ebde51537d6ffba9b85d270e101f255d6
夏宁 <478084629@qq.com>
2023-12-06 01:02:33 +0800

    1

--- src/views/homeHooks.js
+++ src/views/homeHooks.js
@@ -1,11 +1,5 @@
 import { onMounted, ref, onUnmounted } from 'vue'
 
-// const a = ref(1)
-
-// onMounted(() => {
-//   console.log(a)
-// })
-
 export const useHome = val => {
   const a = ref(val)
 
